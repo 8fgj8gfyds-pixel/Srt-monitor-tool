255ee6348f985658b4ee13d288e79e575402ef15
8fgj8gfyds-pixel <8fgj8gfyds@privaterelay.appleid.com>
2026-07-09 08:02:00 -0700

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,7 @@
+# SRT Monitor Configuration
+SECRET_KEY = 'change-this-secret-key-in-production'
+DEBUG = False
+HOST = '0.0.0.0'
+PORT = 5000
+LOG_FILE = '/var/log/srt-monitor/app.log'
+DATA_DIR = '/var/lib/srt-monitor'
